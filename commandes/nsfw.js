7fc0e553e351d414913fb2437df9846a7a683b5c
Jika <jikamdr@gmail.com>
2021-02-03 19:07:17 +0100

    nsfw justalk/pornhub-api

--- commandes/nsfw.js
+++ commandes/nsfw.js
@@ -1,4 +1,4 @@
-const nsfw = require('pornsearch');
+const nsfw = require('@justalk/pornhub-api');
 const Discord = require('discord.js');
 
 exports.run = async (client, msg, args, ops) => {
@@ -8,27 +8,15 @@ exports.run = async (client, msg, args, ops) => {
     if (args[0] === 'video') {
         args.splice(0, 1);
         if (!args[0]) return msg.channel.send("Utilisation de la commande nsfw ```nsfw <gif|video> (defaut gif) <recherche mots clés Pornhub.com>```");
-        const search = new nsfw.search(args);
-        search.gifs()
-        .then(gifs => {
-            let rnd = Math.floor(Math.random() * Math.floor(gifs.length-1));
-            msg.channel.send(`:underage: NSFW :underage: \n ${gifs[rnd].webm}`);
-        });
+        const search = await nsfw.search(args, ["link"], {page: 2});
+        let rnd = Math.floor(Math.random() * Math.floor(search.length-1));
+        msg.channel.send(`:underage: NSFW :underage: \n ${search[rnd].link}`);
     } else {
         if (args[0] === 'gif') { args.splice(0, 1); }
         if (!args[0]) return msg.channel.send("Utilisation de la commande nsfw ```nsfw <gif|video> (defaut gif) <recherche mots clés Pornhub.com>```");
-        const search = new nsfw.search(args);
-        search.gifs()
-        .then(gifs => {
-            let rnd = Math.floor(Math.random() * Math.floor(gifs.length-1));
-            msg.channel.send(`:underage: NSFW :underage: \n ${gifs[rnd].url}`);
-            /*let emb = new Discord.MessageEmbed()
-            .addField(':underage: NSFW :underage:', `Lien : '${gifs[rnd].url}'`)
-            .setImage(`${gifs[rnd].url}`)
-            .setColor([Math.round(Math.random()*255), Math.round(Math.random()*255), Math.round(Math.random()*255)])
-            .setFooter('Gif fourni par Pornhub.com', `${msg.author.displayAvatarURL()}`);
-            msg.channel.send({embed : emb});*/
-        });
+        const search = await nsfw.search(args, ["link_webm"], {page: 2, search: 'gifs'});
+        let rnd = Math.floor(Math.random() * Math.floor(search.length-1));
+        msg.channel.send(`:underage: NSFW :underage: \n ${search[rnd].link_webm}`);
     }
 
 }
