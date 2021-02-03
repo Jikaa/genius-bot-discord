3f7d73d5cd07e51040f34d1f2c6d3c15726b879c
Jika <jikamdr@gmail.com>
2021-02-03 19:18:27 +0100

    nsfw justalk/pornhub-api

--- commandes/nsfw.js
+++ commandes/nsfw.js
@@ -1,4 +1,5 @@
 const nsfw = require('@justalk/pornhub-api');
+const { scraping_search } = require('@justalk/pornhub-api/src/search');
 const Discord = require('discord.js');
 
 exports.run = async (client, msg, args, ops) => {
@@ -8,15 +9,19 @@ exports.run = async (client, msg, args, ops) => {
     if (args[0] === 'video') {
         args.splice(0, 1);
         if (!args[0]) return msg.channel.send("Utilisation de la commande nsfw ```nsfw <gif|video> (defaut gif) <recherche mots clés Pornhub.com>```");
-        const search = await nsfw.search(args, ["link"], {page: 2});
-        let rnd = Math.floor(Math.random() * Math.floor(search.length-1));
-        msg.channel.send(`:underage: NSFW :underage: \n ${search[rnd].link}`);
+        const search = await nsfw.search(args, ["link"], {page: 2})
+        .then(videos => {
+            let rnd = Math.floor(Math.random() * Math.floor(videos.length-1));
+            msg.channel.send(`:underage: NSFW :underage: \n ${videos[rnd].link}`);
+        });
     } else {
         if (args[0] === 'gif') { args.splice(0, 1); }
         if (!args[0]) return msg.channel.send("Utilisation de la commande nsfw ```nsfw <gif|video> (defaut gif) <recherche mots clés Pornhub.com>```");
-        const search = await nsfw.search(args, ["link_webm"], {page: 2, search: 'gifs'});
-        let rnd = Math.floor(Math.random() * Math.floor(search.length-1));
-        msg.channel.send(`:underage: NSFW :underage: \n ${search[rnd].link_webm}`);
+        const search = await nsfw.search(args, ["link_webm"], {page: 2, search: 'gifs'})
+        .then(gifs => {
+            let rnd = Math.floor(Math.random() * Math.floor(gifs.length-1));
+            msg.channel.send(`:underage: NSFW :underage: \n ${gifs[rnd].link_webm}`);
+        });
     }
 
 }
